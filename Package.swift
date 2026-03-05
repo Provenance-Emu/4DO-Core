6e9195e5874d8aee96fd91462315e3a28f1721dd
Joseph Mattiello <git@joemattiello.com>
2026-03-04 23:28:05 -0500

    build: set iOS/tvOS minimum to v17

--- Package.swift
+++ Package.swift
@@ -34,8 +34,8 @@ let package = Package(
     name: "PVCore4DO",
     defaultLocalization: "en",
     platforms: [
-        .iOS(.v16),
-        .tvOS(.v16),
+        .iOS(.v17),
+        .tvOS(.v17),
         .watchOS(.v9),
         .macOS(.v11),
         .macCatalyst(.v17),
